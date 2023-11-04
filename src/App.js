8ecbfcfb56672a60cfee9bfdb251f06266532eb3
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2023-11-04 10:12:25 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -40,7 +40,14 @@ export default function App() {
   }
 
   function handleSplitBill(value) {
-    console.log(value);
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: (friend.balance += value) }
+          : friend
+      )
+    );
+    setSelectedFriend(null);
   }
 
   return (
